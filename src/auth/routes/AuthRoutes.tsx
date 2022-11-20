44167620f9e4f7dd01d1202c64768213398f7dd4
Carlos Raúl Cárcamo Tejada <carlosraulcarcamotejada@gmail.com>
2022-11-19 18:09:13 -0600

    Avance para despliegue en Netlify

--- src/auth/routes/AuthRoutes.tsx
+++ src/auth/routes/AuthRoutes.tsx
@@ -1,11 +1,11 @@
 import { FC } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { SignUpPage, ForgotPasswordPage, SignInPage } from "../pages";
+import { SignUpPage, ForgotPasswordPage, SignIn } from "../pages";
 
 export const AuthRoutes: FC = (): JSX.Element => {
   return (
     <Routes>
-      <Route path="/signin" element={<SignInPage />} />
+      <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
       <Route path="/*" element={<Navigate to="/signin" />} />
