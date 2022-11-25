2e587abe643cfc49ddc6e104429ff9c81518aec1
Carlos Raúl Cárcamo Tejada <carlosraulcarcamotejada@gmail.com>
2022-11-24 19:12:45 -0600

    Avances

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scritp to build the splash screens
+
+npx pwa-asset-generator public/todoist-logo.svg public -m public/site.webmanifest --padding "calc(50vh - 40%) calc(50vw - 40%)" -b "#0f172a" -q 100 -i public/asset-generator-changes.html --favicon
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header: FC<props> = ({ children }): JSX.Element => {
   return (
     <header
       className="standalone:pt-safe-top standalone:pb-4 py-2 fixed z-10 top-0 right-0 w-full bg-gradient-to-r from-emerald-400 to-indigo-400 flex justify-center items-center shadow-sm
-      dark:from-green-600 dark:to-indigo-600
+      dark:from-emerald-600 dark:to-indigo-600
       "
     >
       <div className="mt-2">
