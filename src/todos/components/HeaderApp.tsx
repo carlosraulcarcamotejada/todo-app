8dabaa47a90af5c8b5ce209919908b5135ecee62
Carlos Raúl Cárcamo Tejada <carlosraulcarcamotejada@gmail.com>
2022-12-04 14:48:30 -0600

    Cambios en headerd

--- src/todos/components/HeaderApp.tsx
+++ src/todos/components/HeaderApp.tsx
@@ -9,9 +9,9 @@ export const HeaderApp: FC = (): JSX.Element => {
   const userUrlImage = 'https://yt3.ggpht.com/ytc/AMLnZu-fRDgqki18U5Qd9mEzIUW_1JnPLbXE7hJ5WjEpnw=s108-c-k-c0x00ffffff-no-rj'
 
   return (
-    <div className="flex shadow-md text-white items-center justify-start w-full h-48 fixed top-0 right-0 rounded-b-3xl bg-teal-500">
+    <div className="flex shadow-md text-white items-center justify-start w-full h-36 fixed top-0 right-0 rounded-b-3xl bg-teal-500">
       <div className="ml-6">
-        <h2 className="text-2xl mb-1 font-semibold">HI! {name || 'Carlos Raúl'}</h2>
+        <h2 className="text-2xl mb-1 font-semibold">HI! {name || 'Alex Alonzo'}</h2>
         <p className="text-md">There are {things} important things... </p>
       </div>
       <div className="ml-12 flex justify-end items-end">
--- src/todos/components/Navbar.tsx
+++ src/todos/components/Navbar.tsx
@@ -15,76 +15,76 @@ import {
 import { NavLink } from "react-router-dom";
 
 export const Navbar: FC = (): JSX.Element => {
-
-
   return (
-    <div className="flex fixed w-full shadow-md bottom-0 h-22 bg-white dark:bg-gray-900 rounded-tl-3xl rounded-tr-3xl">
+    <div className="flex fixed w-full p-2 shadow-md bottom-0 h-20 bg-white dark:bg-gray-900 rounded-tl-3xl rounded-tr-3xl">
       <div className="flex relative justify-around items-center w-full">
         <NavLink
-        className="h-14 w-14 flex justify-center items-center"
-        to="/home">
+          className="h-16 w-16 flex justify-center items-center "
+          to="/home"
+        >
           {({ isActive }) => {
             return isActive ? (
               <HomeIconSolid
-                className={`h-8 w-8  text-teal-500  active:scale-90 active:text-teal-700 transition-all duration-200`}
+                className={`h-16 w-16 p-4 text-teal-500  active:scale-90 active:text-teal-700 transition-all duration-200`}
               />
             ) : (
               <HomeIcon
-                className={`h-8 w-8 text-neutral-700/70 dark:text-neutral-200/70 active:text-neutral-800/70 active:dark:text-neutral-300/70 active:scale-90 transition-all duration-200`}
+                className={`h-16 w-16 p-4 text-neutral-700/70 dark:text-neutral-200/70 active:text-neutral-800/70 active:dark:text-neutral-300/70 active:scale-90 transition-all duration-200`}
               />
             );
           }}
         </NavLink>
-        <div className="mr-6">
-        <NavLink to="/page2"
-        className="h-14 w-14  flex justify-center items-center" 
+
+        <NavLink
+          to="/page2"
+          className="h-16 w-16 flex justify-center items-center "
         >
           {({ isActive }) => {
             return isActive ? (
               <CalendarDaysIconSolid
-                className={`h-8 w-8  text-teal-500  active:scale-90 active:text-teal-700 transition-all duration-200`}
+                className={`h-16 w-16 p-4 text-teal-500  active:scale-90 active:text-teal-700 transition-all duration-200`}
               />
             ) : (
               <CalendarDaysIcon
-                className={`h-8 w-8 text-neutral-700/70 dark:text-neutral-200/70 active:text-neutral-800/70 active:dark:text-neutral-300/70 active:scale-90 transition-all duration-200`}
+                className={`h-16 w-16 p-4 text-neutral-700/70 dark:text-neutral-200/70 active:text-neutral-800/70 active:dark:text-neutral-300/70 active:scale-90 transition-all duration-200`}
               />
             );
           }}
         </NavLink>
-        </div>
 
-        <div className="flex absolute -top-8 justify-center items-center">
+        <div className="flex relative -top-10 justify-center items-center">
           <FloatinButton />
         </div>
 
-        <div className="ml-6">
-          <NavLink to="/page3"
-           className="h-14 w-14 flex justify-center items-center"
-          >
-            {({ isActive }) => {
-              return isActive ? (
-                <UserCircleIconSolid
-                  className={`h-8 w-8  text-teal-500  active:scale-90 active:text-teal-700 transition-all duration-200`}
-                />
-              ) : (
-                <UserCircleIcon
-                  className={`h-8 w-8 text-neutral-700/70 dark:text-neutral-200/70 active:text-neutral-800/70 active:dark:text-neutral-300/70 active:scale-90 transition-all duration-200`}
-                />
-              );
-            }}
-          </NavLink>
-        </div>
-        <NavLink to="/page4"
-         className="h-14 w-14 flex justify-center items-center"
+        <NavLink
+          to="/page3"
+          className="h-16 w-16 flex justify-center items-center"
+        >
+          {({ isActive }) => {
+            return isActive ? (
+              <UserCircleIconSolid
+                className={`h-16 w-16 p-4  text-teal-500  active:scale-90 active:text-teal-700 transition-all duration-200`}
+              />
+            ) : (
+              <UserCircleIcon
+                className={`h-16 w-16 p-4 text-neutral-700/70 dark:text-neutral-200/70 active:text-neutral-800/70 active:dark:text-neutral-300/70 active:scale-90 transition-all duration-200`}
+              />
+            );
+          }}
+        </NavLink>
+
+        <NavLink
+          to="/page4"
+          className="h-16 w-16 flex justify-center items-center "
         >
           {({ isActive }) => {
             return isActive ? (
               <Cog6ToothIconSolid
-                className={`h-8 w-8  text-teal-500  active:scale-90 active:text-teal-700 transition-all duration-200`}
+                className={`h-16 w-16 p-4 text-teal-500  active:scale-90 active:text-teal-700 transition-all duration-200`}
               />
             ) : (
               <Cog6ToothIcon
-                className={`h-8 w-8 text-neutral-700/70 dark:text-neutral-200/70 active:text-neutral-800/70 active:dark:text-neutral-300/70 active:scale-90 transition-all duration-200`}
+                className={`h-16 w-16 p-4 text-neutral-700/70 dark:text-neutral-200/70 active:text-neutral-800/70 active:dark:text-neutral-300/70 active:scale-90 transition-all duration-200`}
               />
             );
           }}
@@ -101,7 +101,35 @@ const FloatinButton: FC = (): JSX.Element => {
       rounded-full text-neutral-100 transition-all duration-200 bg-teal-500 active:bg-teal-600 active:scale-90"
       type="button"
     >
-      <PlusIcon className="h-8 w-8 " />
+      <PlusIcon className="h-8 w-8" />
     </button>
   );
 };
+
+// type NavItemProps = {
+//   ActiveIcon: (<CalendarDaysIcon />) | ( <UserCircleIcon /> ) | ( <Cog6ToothIcon />)  |( <HomeIcon />)
+//   InactiveIcon: JSX.Element;
+//   path: string;
+// };
+
+// const NavItem: FC<NavItemProps> = ({
+//   ActiveIcon,
+//   InactiveIcon,
+//   path,
+// }): JSX.Element => {
+//   return (
+//     <NavLink className="h-16 w-16 flex justify-center items-center " to={path}>
+//       {({ isActive }) => {
+//         return isActive ? (
+//           <ActiveIcon
+//             className={`h-16 w-16 p-4 text-teal-500  active:scale-90 active:text-teal-700 transition-all duration-200`}
+//           />
+//         ) : (
+//           <InactiveIcon
+//             className={`h-16 w-16 p-4 text-neutral-700/70 dark:text-neutral-200/70 active:text-neutral-800/70 active:dark:text-neutral-300/70 active:scale-90 transition-all duration-200`}
+//           />
+//         );
+//       }}
+//     </NavLink>
+//   );
+// };
